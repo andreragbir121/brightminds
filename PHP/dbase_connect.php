0332c6ff46d74092e65ea04dcbd5c9a4e5b804f3
Andre Ragbir <andreragbir121@gmail.com>
2026-01-22 11:13:28 -0400

    Update database connection details for production environment

--- PHP/dbase_connect.php
+++ PHP/dbase_connect.php
@@ -1,9 +1,9 @@
 <?php
    // Database connection
-   $server = 'localhost'; 
-   $user = 'root'; 
-   $password = '';
-   $database = 'brightminds';
+   $server = 'sql311.byethost3.com'; 
+   $user = 'b3_40935809'; 
+   $password = 'CVpYA90hKZapo7';
+   $database = 'b3_40935809_brightminds';
 
    $conn = mysqli_connect($server, $user, $password, $database); 
 
